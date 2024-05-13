9ce36415be598a903806395d9381ddfcc979dd9a
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-13 15:12:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function clearScreen(){
-    document.getElementById(result).value = "";
+    document.getElementById('result').value = "";
 }
 
 function display(value){
-    document.getElementById(result).value += value;
+    document.getElementById('result').value += value;
+}
+
+function calculate(){
+    let sum = document.getElementById('result').value;
+    let answer = eval(sum);
+    document.getElementById('result').value = answer;
 }
