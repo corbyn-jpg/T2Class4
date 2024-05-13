9af8ed6c6cf92e4e0c74349c957cc3781d629ef8
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-13 15:30:34 +0200

    let form

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ function calculate(){
     let answer = eval(sum);
     document.getElementById('result').value = answer;
 }
+
+let form = document.forms['my-form'];
+form.addEventListener("submit", getValues);
+
+function getValues(event){
+    event.preventDefaults();
+
+    let formData = {
+        "name": this.name.value;
+        "bio": this.bio.value;
+        "favColour": this['fav-color'].value;
+        "gender": this.gender.value;
+    }
+}
